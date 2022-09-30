d8cfc39c414c3ff9e8c0d82627b0174c63d8184a
Gleb Kochubey <hlib.kochubei@gmail.com>
2022-09-30 22:55:17 +0200

    Add 'my_reduce' function

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -19,7 +19,21 @@ module Exercise
       def my_compact; end
 
       # Написать свою функцию my_reduce
-      def my_reduce; end
+      def my_reduce(initial_value = nil, &func)
+        if initial_value.nil?
+          ignore_first = true
+          initial_value = first
+        end
+
+        iter = lambda { |accumulator, index|
+          return accumulator if index == size
+
+          accumulator = func.call(accumulator, self[index]) unless ignore_first && index.zero?
+          iter.call(accumulator, index + 1)
+        }
+
+        iter.call(initial_value, 0)
+      end
     end
   end
 end

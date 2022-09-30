f679407bf33b6b100291c83134f69d7cd68b68d1
Gleb Kochubey <hlib.kochubei@gmail.com>
2022-09-30 15:26:00 +0200

    Add 'rating' function

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,8 +4,10 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        selected_films = array.reject { |film| film['rating_kinopoisk'].to_f.zero? }.select { |film| film['country'].to_s.split(',').size > 1 }
+        accumulate_rating = selected_films.reduce(0) { |acc, film| acc + film['rating_kinopoisk'].to_f }
+        accumulate_rating / selected_films.size
       end
 
       def chars_count(_films, _threshold)

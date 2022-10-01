8766be916b61980c28146144b14da7793f3f11ca
Gleb Kochubey <hlib.kochubei@gmail.com>
2022-10-01 23:08:43 +0200

    Implement 'rating' function using '.map' method

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -6,7 +6,7 @@ module Exercise
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(array)
         selected_films = array.reject { |film| film['rating_kinopoisk'].to_f.zero? }.select { |film| film['country'].to_s.split(',').size > 1 }
-        accumulate_rating = selected_films.reduce(0) { |acc, film| acc + film['rating_kinopoisk'].to_f }
+        accumulate_rating = selected_films.map { |film| film['rating_kinopoisk'].to_f }.reduce(:+)
         accumulate_rating / selected_films.size
       end
 

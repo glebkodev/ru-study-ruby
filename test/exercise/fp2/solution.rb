3ba731022932ad3e905aa4b9ad3f20036a01325c
Gleb Kochubey <hlib.kochubei@gmail.com>
2022-10-02 14:36:36 +0200

    Refactoring the 'my_reduce' function using recursion

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,8 +5,8 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each(array = nil, &func)
-        head, *tail = array || self
+      def my_each(array = self, &func)
+        head, *tail = array
         func.call(head)
         return self if tail.empty?
 
@@ -27,20 +27,12 @@ module Exercise
       end
 
       # Написать свою функцию my_reduce
-      def my_reduce(initial_value = nil, &func)
-        if initial_value.nil?
-          ignore_first = true
-          initial_value = first
-        end
+      def my_reduce(accumulator = nil, &func)
+        return accumulator if empty?
 
-        iter = lambda { |accumulator, index|
-          return accumulator if index == size
-
-          accumulator = func.call(accumulator, self[index]) unless ignore_first && index.zero?
-          iter.call(accumulator, index + 1)
-        }
-
-        iter.call(initial_value, 0)
+        head, *tail = self
+        accumulator = accumulator.nil? ? head : func.call(accumulator, head)
+        MyArray.new(tail).my_reduce(accumulator, &func)
       end
     end
   end
